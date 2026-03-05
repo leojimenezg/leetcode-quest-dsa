70f972c5366413b26f79458e7e6dccc74f27c686
leojimenezg <leonardojimenezgz@gmail.com>
2026-03-05 11:22:14 -0600

    feat: first problem of Binary Search

---
+++ solutions/BinarySearch.go
@@ -0,0 +1,31 @@
+// ============================================================================
+// Binary Seach
+// ============================================================================
+
+package solutions
+
+// Peak Index in Mountain Array
+// Patrones:
+//   - Binary Search
+//
+// Útil cuando:
+//   - se busca un punto de inflexión en una secuencia unimodal
+//   - se pueden descartar mitades basándose en la pendiente local
+//   - se requiere O(log n) en lugar de O(n)
+//
+// Complejidad:
+//   - Tiempo: O(log n)
+//   - Espacio: O(1)
+func peakIndexInMountainArray(arr []int) int {
+	left := 0
+	right := len(arr)
+	for (right - left) > 1 {
+		mid := (left + right) / 2 - 1
+		if arr[mid] < arr[mid+1] {
+			left = mid + 1
+		} else {
+			right = mid + 1
+		}
+	}
+	return left
+}
