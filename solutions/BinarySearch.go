082be7a72b9e56a3324518aaf367eb3465e243c4
leojimenezg <leonardojimenezgz@gmail.com>
2026-03-06 11:30:37 -0600

    feat: second problem of Binary Search

--- solutions/BinarySearch.go
+++ solutions/BinarySearch.go
@@ -29,3 +29,34 @@ func peakIndexInMountainArray(arr []int) int {
 	}
 	return left
 }
+
+// Binary Search
+// Patrones:
+//   - Binary Search
+//
+// Útil cuando:
+//   - se busca un elemento específico en un arreglo ordenado
+//   - se pueden descartar mitades basándose en la comparación con el target
+//   - se requiere O(log n) en lugar de O(n)
+//
+// Complejidad:
+//   - Tiempo: O(log n)
+//   - Espacio: O(1)
+func search(nums []int, target int) int {
+	left := 0
+	right := len(nums)
+	for (right - left) > 1 {
+		mid := (left + right) / 2 - 1
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+	if nums[left] == target {
+		return left
+	}
+	return -1
+}
