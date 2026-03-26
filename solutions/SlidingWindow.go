ee70965dfcf7c071de9a94bb1738f848027ebf5f
leojimenezg <leonardojimenezgz@gmail.com>
2026-03-26 13:29:47 -0600

    feat: second problem of Sliding Window

--- solutions/SlidingWindow.go
+++ solutions/SlidingWindow.go
@@ -25,3 +25,32 @@ func containsNearbyDuplicate(nums []int, k int) bool {
 	}
 	return false
 }
+
+// Number of Substrings Containing All Three Characters
+// Patrones:
+//   - Sliding Window
+//   - Array as Hash
+//
+// Útil cuando:
+//   - se cuentan substrings que satisfacen una condición sobre caracteres
+//   - el conjunto de caracteres es pequeño y conocido
+//   - todos los substrings a la derecha de una ventana válida también son válidos
+//
+// Complejidad:
+//   - Tiempo: O(n)
+//   - Espacio: O(1)
+func numberOfSubstrings(s string) int {
+	n := len(s)
+	present := [3]int{} // [a, b, c]
+	left, right, count := 0, 0, 0
+	for right < n {
+		present[s[right]-'a']++
+		for present[0] > 0 && present[1] > 0 && present[2] > 0 {
+			count += n - right
+			present[s[left]-'a']--
+			left++
+		}
+		right++
+	}
+	return count
+}
