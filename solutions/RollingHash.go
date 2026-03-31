59bc35e0b11505031a03938fbedaa4a6d2340334
leojimenezg <leonardojimenezgz@gmail.com>
2026-03-31 10:26:20 -0600

    feat: second problem of Rolling Hash

--- solutions/RollingHash.go
+++ solutions/RollingHash.go
@@ -39,3 +39,42 @@ func shortestPalindrome(s string) string {
 	}
 	return string(reversed) + s
 }
+
+// Longest Happy Prefix
+// Patrones:
+//   - Rolling Hash
+//   - Polynomial Hashing
+//
+// Útil cuando:
+//   - se busca el prefijo más largo que también es sufijo
+//   - se necesita comparar substrings eficientemente en O(1)
+//   - la verificación directa de strings daría O(n^2)
+//
+// Notas:
+//   - prefixHash crece de izquierda a derecha: hash = hash*p + val(s[i])
+//   - suffixHash crece de derecha a izquierda: hash = hash + val(s[j])*p^i
+//   - prefixHash == suffixHash implica que el prefijo s[:i+1] es igual al sufijo s[n-i-1:]
+//
+// Complejidad:
+//   - Tiempo: O(n)
+//   - Espacio: O(1)
+func longestPrefix(s string) string {
+	n := len(s)
+	if n == 0 {
+		return ""
+	}
+	p, mod, power := 31, 1_000_000_007, 1
+	prefixHash, suffixHash, longestIdx := 0, 0, -1
+	for i, j := 0, n-1; i < n-1 && j > 0; i, j = i+1, j-1 {
+		prefixHash = (prefixHash*p + int(s[i])) % mod
+		suffixHash = (suffixHash + int(s[j])*power) % mod
+		power = (power * p) % mod
+		if prefixHash == suffixHash {
+			longestIdx = i
+		}
+	}
+	if longestIdx < 0 {
+		return ""
+	}
+	return s[:longestIdx+1]
+}
