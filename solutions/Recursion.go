b76d6447dfaa54ec5d24ce9b29608b96a845d745
leojimenezg <leonardojimenezgz@gmail.com>
2026-04-07 11:24:30 -0600

    feat: first problem of Recursion

---
+++ solutions/Recursion.go
@@ -0,0 +1,31 @@
+package solutions
+
+// Merge Two Sorted Lists
+//
+// Patrones:
+//   - Recursion
+//   - Pointer Rewiring
+//
+// Útil cuando:
+//   - se fusionan dos listas ordenadas nodo por nodo
+//   - se puede delegar el resto del problema a una llamada recursiva
+//   - el caso base es cuando una lista se agota
+//
+// Complejidad:
+//   - Tiempo: O(m+n)
+//   - Espacio: O(m+n)
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+	if list1.Val < list2.Val {
+		list1.Next = mergeTwoLists(list1.Next, list2)
+		return list1
+	} else {
+		list2.Next = mergeTwoLists(list1, list2.Next)
+		return list2
+	}
+}
