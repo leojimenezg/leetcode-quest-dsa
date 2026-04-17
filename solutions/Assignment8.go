e12c1f5424261252f2b3ce99af3080b5659bc6af
leojimenezg <leonardojimenezgz@gmail.com>
2026-04-17 12:06:23 -0600

    feat: last problem of Assignment 8

--- solutions/Assignment8.go
+++ solutions/Assignment8.go
@@ -31,3 +31,37 @@ func numOfSubarrays(arr []int, k int, threshold int) int {
 	}
 	return total
 }
+
+// Grumpy Bookstore Owner
+//
+// Patrones:
+//   - Sliding Window (Fixed Size)
+//
+// Útil cuando:
+//   - hay una base fija de satisfacción independiente de la técnica
+//   - se busca maximizar el beneficio adicional en una ventana de tamaño fijo
+//   - se pueden separar los dos componentes del resultado y calcularlos independientemente
+//
+// Complejidad:
+//   - Tiempo: O(n)
+//   - Espacio: O(1)
+func maxSatisfied(customers []int, grumpy []int, minutes int) int {
+	n := len(customers)
+	left, sumWindow := 0, 0
+	satisfied, technique := 0, 0
+	for right := range n {
+		if grumpy[right] == 1 {
+			sumWindow += customers[right]
+		} else {
+			satisfied += customers[right]
+		}
+		if right-left+1 > minutes {
+			if grumpy[left] == 1 {
+				sumWindow -= customers[left]
+			}
+			left++
+		}
+		technique = max(technique, sumWindow)
+	}
+	return satisfied + technique
+}
